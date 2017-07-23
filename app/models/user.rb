e64c8830878663a8245c2fa0b45bd9c55fda5cd8
Sean Ross <seanmross9@gmail.com>
2017-07-23 14:07:07 -0400

    created UserController and HTTP basic authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :lists
+
+  #has_secure_password invokes password_digest
+  #comment out when testing password in console
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#about'
   get 'welcome/about'
   get 'welcome/index'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
+
 end
