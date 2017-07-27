157be15e7fa492373b2604344eddd17d9d65d239
Sean Ross <seanmross9@gmail.com>
2017-07-26 23:56:13 -0400

    implemented api/lists#destroy and api/users#destroy

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,13 +1,27 @@
 class Api::ListsController < ApiController
   before_action :authenticated?
 
+  def index
+    lists = List.all
+    render json: lists, each_serializer: ListSerializer
+  end
+
   def create
     list = List.new(params.require(:list).permit(:name, :private))
-
     if list.save
       render json: list
     else
       render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
     end
   end
 
